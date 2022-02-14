3f403ef60f3a2b851272de629673cb6c7423a363
eungbean1029 <dana5355@naver.com>
2022-02-15 00:34:42 +0900

    coin 프로젝트 생성

---
+++ src/api.ts
@@ -0,0 +1,20 @@
+const BASE_URL = `https://api.coinpaprika.com/v1`;
+
+export function fetchCoins(){
+    return fetch(`${BASE_URL}/coins`).then((response) =>
+     response.json()
+    );
+}
+
+export function fetchCoinInfo(coinID:string){
+    return fetch(`${BASE_URL}/coins/${coinID}`).then((response) =>
+     response.json()
+    );
+}
+
+export function fetchCoinTickers(coinID:string){
+    return fetch(`${BASE_URL}/tickers/${coinID}`).then((response) =>
+     response.json()
+    );
+}
+
